3a41d9c1942f7d1e04931468fb17231b30aed8d7
LBRN <111892485+Panginoon@users.noreply.github.com>
2024-11-03 21:47:08 +0800

    Add files via upload

---
+++ React-TodoApp/maranan-midterms/src/constants/Data.jsx
@@ -0,0 +1,8 @@
+export const catagory = [
+    { id: 1, catagory: "Home"},
+    { id: 2, catagory: "Work"},
+    { id: 3, catagory: "Personal"},
+    { id: 4, catagory: "Health/Fitness"},
+    { id: 5, catagory: "Education"},
+    { id: 6, catagory: "Gym"},
+  ];
